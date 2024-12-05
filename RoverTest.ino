dbb7f880e90f383173358e4cdc8c5d39290c3ad7
siang <sochin1519@eagle.fgcu.edu>
2024-12-05 10:11:05 -0500

    Update RoverTest.ino

--- RoverTest.ino
+++ RoverTest.ino
@@ -8,15 +8,15 @@
 // with direction of the head and distance measured.
 // ------------------------------------------------------------
 
-#include <LiquidCrystal.h>
+// #include <LiquidCrystal.h>
 
 #include "RoverHead.h"
 #include "RoverWheels.h"
 #include "StopWatch.h"
 
 // LCD
-const int rs = 12, en = 11, d4 = 3, d5 = 2, d6 = 1, d7 = 0;
-LiquidCrystal lcd(rs, en, d4, d5, d6, d7);
+// const int rs = 12, en = 11, d4 = 3, d5 = 2, d6 = 1, d7 = 0;
+// LiquidCrystal lcd(rs, en, d4, d5, d6, d7);
 
 
 fgcu::FourPin motorPinsLeft { A0, A1, A2, A3};
@@ -30,6 +30,15 @@ const byte TriggerPin = A4;
 
 fgcu::RoverHead head{EchoPin, TriggerPin, ServoPin};
 
+const float TURN_DEGREE = 1;  // Turn complete 90 degrees
+
+enum STATE {
+  NORMAL = 0,
+  FORCE_FORWARD = 1
+};
+
+byte curState = NORMAL;
+
 enum ANGLE {
   LEFT = 8,
   STRAIGHT = 98,
@@ -42,14 +51,13 @@ const float moveDist = 4;
 const float minDist = 10;
 
 void setup() {
-  lcd.begin(16,2);
-  lcd.write("Hello world!");
+  // lcd.begin(16,2);
+  // lcd.write("Hello world!");
 
   delay(2000);
-
-  lcd.clear();
-
   head.turnHead(STRAIGHT);
+  while(!head.run());
+  // lcd.clear();
 }
 
 
@@ -57,32 +65,47 @@ word straightDist{0};
 void loop() {
   wheels.run();
   if(!wheels.isMoving()) {
-    straightDist = getDistanceAtAngle(STRAIGHT);
+    switch(curState) {
+      case NORMAL: {
+        head.turnHead((byte)STRAIGHT);
+        while(!head.run());
+        while(head.isMeasuring()) {
+          head.run();
+        }
+        straightDist = head.getDistance();
 
-    lcd.clear();
-    lcd.setCursor(0, 0);
-    lcd.print("Distance: ");
-    lcd.setCursor(10, 0);
-    lcd.print(straightDist);
+        if(straightDist < minDist) {
+          head.turnHead((byte)RIGHT);
+          while(!head.run());
+          while(head.isMeasuring()) {
+            head.run();
+          }
+          word rDist = head.getDistance();
+        
+          head.turnHead((byte)LEFT);
+          while(head.isMeasuring()) {
+            head.run();
+          }
+          word lDist = head.getDistance();
 
-    if(straightDist < minDist) {
-      if(getDistanceAtAngle(RIGHT) > getDistanceAtAngle(LEFT)) { // left dist > right dist
-        wheels.turnLeft(1.2);
-      } else {
-        wheels.turnRight(1.2);
+          if(rDist > lDist) { // left dist > right dist
+            wheels.turnLeft(TURN_DEGREE);
+            curState = FORCE_FORWARD;
+            straightDist = rDist;
+          } else {
+            wheels.turnRight(TURN_DEGREE);
+            curState = FORCE_FORWARD;
+            straightDist = lDist;
+          }
+        } else {
+          curState = FORCE_FORWARD; // Each full rotation is 8 inches
+        }
+        break;
       }
-    } else {
-      wheels.moveForward(moveDist/8.f); // Each full rotation is 8 inches
+      case FORCE_FORWARD: {
+        wheels.moveForward((straightDist*.60)/8.f); // Each full rotation is 8 inches
+        curState = NORMAL;
+      }
     }
   }
-
 };
-
-word getDistanceAtAngle(byte angle) {
-    // Right check
-  head.turnHead(angle);
-  while(head.isMeasuring()) {
-    head.run();
-  }
-  return head.getDistance();
-}
