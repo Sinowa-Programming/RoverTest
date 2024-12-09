5fc475f08e2823f4fc6c94f553aef615b8626b6b
SiangCh <67034864+Sinowa-Programming@users.noreply.github.com>
2024-12-09 16:03:43 -0500

    Update RoverTest.ino
    Comments

--- RoverTest.ino
+++ RoverTest.ino
@@ -1,22 +1,13 @@
 // RoverTest.ino
 // FGCU Computing & Software Engineering
-// Prof. Allen
-// Test the basic Rover robot interfaces. Uses a set of states
-// to run the wheels through each of the basic movements and
-// adjusts the head to point in the direction of movement. The
-// Serial inteface is used to output direction of movement, along
-// with direction of the head and distance measured.
+// Siang Chin & Sahbah Gonzalez
+// Navigate the rover though a simple course using an ultrasonic sensor
+// to detect walls and 2 steppers motors to move in a controlled fashion.
 // ------------------------------------------------------------
-
-// #include <LiquidCrystal.h>
 
 #include "RoverHead.h"
 #include "RoverWheels.h"
 #include "StopWatch.h"
-
-// LCD
-// const int rs = 12, en = 11, d4 = 3, d5 = 2, d6 = 1, d7 = 0;
-// LiquidCrystal lcd(rs, en, d4, d5, d6, d7);
 
 
 fgcu::FourPin motorPinsLeft { A0, A1, A2, A3};
@@ -51,13 +42,9 @@ const float moveDist = 4;
 const float minDist = 10;
 
 void setup() {
-  // lcd.begin(16,2);
-  // lcd.write("Hello world!");
-
   delay(2000);
   head.turnHead(STRAIGHT);
   while(!head.run());
-  // lcd.clear();
 }
 
 
@@ -88,11 +75,11 @@ void loop() {
 
           if(rDist > lDist) { // left dist > right dist
             wheels.turnLeft(TURN_DEGREE);
-            curState = FORCE_FORWARD;
+            curState = FORCE_FORWARD;  // Make sure the rover moves forward after turning
             straightDist = rDist;
           } else {
             wheels.turnRight(TURN_DEGREE);
-            curState = FORCE_FORWARD;
+            curState = FORCE_FORWARD;   // Make sure the rover moves forward after turning
             straightDist = lDist;
           }
         } else {
@@ -101,7 +88,7 @@ void loop() {
         break;
       }
       case FORCE_FORWARD: {
-        wheels.moveForward((straightDist*.60)/8.f); // Each full rotation is 8 inches
+        wheels.moveForward((straightDist*.60)/8.f); // Each full rotation is 8 inches. Move forward for 60% of the detected distance.
         curState = NORMAL;
       }
     }
